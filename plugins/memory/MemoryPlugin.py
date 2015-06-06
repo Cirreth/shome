c9debc765dde0fa46b85db6f95c19c025844a22e
Кирилл <cirreth@gmail.com>
2015-06-06 09:51:49 +0300

    MemoryPlugin

--- plugins/memory/MemoryPlugin.py
+++ plugins/memory/MemoryPlugin.py
@@ -9,41 +9,38 @@ class MemoryPlugin(SHomePlugin):
     def __init__(self, params):
         self._filename = params['filename'] if 'filename' in params else 'memory.db'
         try:
-            if self._filename in os.listdir(''):
+            if self._filename not in os.listdir('.'):
                 self._connection = self.__create_database()
-            self._connection = sqlite3.connect('memory.db')
+            self._connection = sqlite3.connect('memory.db', check_same_thread=False)
         except sqlite3.Error as e:
             logging.error('Memory database initalization error: '+str(e))
             raise e
 
     def __create_database(self):
-        try:
-            conn = sqlite3.connect(self._filename)
-            cur = conn.cursor()
-            cur.execute('CREATE TABLE memory (parameter TEXT PRIMARY KEY, value TEXT)')
-            cur.commit()
-        except Exception as e:
-            logging.error('Error on database create: '+str(e))
-            raise e
+        conn = sqlite3.connect(self._filename, check_same_thread=False)
+        cur = conn.cursor()
+        cur.execute('CREATE TABLE memory (parameter TEXT PRIMARY KEY, value TEXT)')
+        conn.commit()
         return conn
 
     def _read(self, parameter):
         cur = self._connection.cursor()
         rs = cur.execute('SELECT value FROM memory WHERE parameter = ?', (parameter,))
-        return rs.fetchone()[0]
+        res = rs.fetchone()
+        return res[0] if res else ''
 
     def _write(self, parameter, value):
         try:
             cur = self._connection.cursor()
             cur.execute('INSERT OR REPLACE INTO memory (parameter, value) VALUES (?, ?)', (parameter, value, ))
-            cur.commit()
+            self._connection.commit()
         except Exception as e:
-            cur.rollback()
-            logging.error('Error on write in memory plugin operation: '+ str(e))
+            self._connection.rollback()
+            logging.error('Error on write in memory plugin operation: '+str(e))
             raise e
 
     def call(self, reference, value=None):
-        if value is not None:
+        if value is not None and value != '':
             self._write(reference, value)
         else:
             return self._read(reference)
